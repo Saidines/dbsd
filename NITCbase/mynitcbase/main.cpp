7d5d6b5f13b32f95862a386c762f8da1c8a2220f
saidines <saidineshreddykukkala@gmail.com>
2026-01-06 19:48:02 +0530

    Revert "stage 1"
    This reverts commit 961022d449b45050ccd0d8a6484c83b9d360b168.

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -2,52 +2,16 @@
 #include "Cache/OpenRelTable.h"
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
-#include "bits/stdc++.h"
-
-int s_o_b(int blockNumber){
-	if(blockNumber>8192) return -1;
-	int mapBlockNumber=blockNumber/2048;
-	blockNumber=blockNumber%20448;
-	unsigned char buffer[BLOCK_SIZE];
-	Disk::readBlock(buffer,mapBlockNumber);
-	return buffer[blockNumber];
-}
 
 int main(int argc, char *argv[]) {
   Disk disk_run;
+<<<<<<< HEAD
 
-  // create objects for the relation catalog and attribute catalog
-  RecBuffer relCatBuffer(RELCAT_BLOCK);
-  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
 
-  HeadInfo relCatHeader;
-  HeadInfo attrCatHeader;
+=======
+  // StaticBuffer buffer;
+  // OpenRelTable cache;
 
-  // load the headers of both the blocks into relCatHeader and attrCatHeader.
-  // (we will implement these functions later)
-  relCatBuffer.getHeader(&relCatHeader);
-  attrCatBuffer.getHeader(&attrCatHeader);
-int numOfRelations = relCatHeader.numEntries;
-int numOfAttributes = attrCatHeader.numEntries;
-  for (int i=0;i<numOfRelations;i++) {
-
-    Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
-
-    relCatBuffer.getRecord(relCatRecord, i);
-
-    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
-
-    for (int j=0;j<numOfAttributes;j++/* j = 0 to number of entries in the attribute catalog */) {
-	    Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
-      // declare attrCatRecord and load the attribute catalog entry into it
-		attrCatBuffer.getRecord(attrCatRecord,j);
-      if (strcmp(attrCatRecord[0].sVal,relCatRecord[0].sVal)==0/* attribute catalog entry corresponds to the current relation */) {
-        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
-        printf("  %s: %s\n", /* get the attribute name */attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
-      }
-    }
-    printf("\n");
-  }
-
-  return 0;
+  return FrontendInterface::handleFrontend(argc, argv);
 }
+>>>>>>> parent of 961022d (stage 1)
