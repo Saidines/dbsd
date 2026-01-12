194f02ac9cdd1397dd0eb02402981b780cfba96e
saidines <saidineshreddykukkala@gmail.com>
2026-01-13 01:28:18 +0530

    stage_4

--- NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
+++ NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
@@ -97,5 +97,47 @@ int BlockBuffer::loadBlockAndGetBufferPtr(unsigned char **bufferPtr){
 
 }	
 
+/* used to get the slotmap from a record block
+NOTE: this function expects the caller to allocate memory for `*slotMap`
+*/
+int RecBuffer::getSlotMap(unsigned char *slotMap) {
+  unsigned char *bufferPtr;
+
+  // get the starting address of the buffer containing the block using loadBlockAndGetBufferPtr().
+  int ret = loadBlockAndGetBufferPtr(&bufferPtr);
+  if (ret != SUCCESS) {
+    return ret;
+  }
+
+  struct HeadInfo head;
+  // get the header of the block using getHeader() function
+	getHeader(&head);
+  int slotCount = head.numSlots;/* number of slots in block from header */;
+
+  // get a pointer to the beginning of the slotmap in memory by offsetting HEADER_SIZE
+  unsigned char *slotMapInBuffer = bufferPtr + HEADER_SIZE;
+	unsigned char*tempSlotMap = (unsigned char*)malloc(slotCount);
+	memcpy(slotMap,slotMapInBuffer,slotCount);
+	
+  // copy the values from `slotMapInBuffer` to `slotMap` (size is `slotCount`)
+
+  return SUCCESS;
+}
 
 
+int compareAttrs(union Attribute attr1,union Attribute attr2,int type){
+	double diff;
+	if(type==0){
+		 diff=attr1.nVal-attr2.nVal;
+	}
+
+	else{
+		diff=strcmp(attr1.sVal,attr2.sVal);
+	}
+
+		if(diff<0) return -1;
+		if(diff>0) return 1;
+		return 0;
+
+
+}
--- NITCbase/mynitcbase/Cache/AttrCacheTable.cpp
+++ NITCbase/mynitcbase/Cache/AttrCacheTable.cpp
@@ -10,7 +10,7 @@ NOTE: this function expects the caller to allocate memory for `*attrCatBuf`
 */
 int AttrCacheTable::getAttrCatEntry(int relId, int attrOffset, AttrCatEntry* attrCatBuf) {
   // check if 0 <= relId < MAX_OPEN and return E_OUTOFBOUND otherwise
-	if(relId<0||relId>MAX_OPEN) {
+	if(relId<0||relId>=MAX_OPEN) {
 		return E_OUTOFBOUND;
 	}
   // check if attrCache[relId] == nullptr and return E_RELNOTOPEN if true
@@ -40,6 +40,32 @@ void AttrCacheTable::recordToAttrCatEntry(union Attribute record[ATTRCAT_NO_ATTR
 	attrCatEntry->rootBlock = record[ATTRCAT_ROOT_BLOCK_INDEX].nVal;
 	attrCatEntry->offset = record[ATTRCAT_OFFSET_INDEX].nVal;
 	strcpy(attrCatEntry->attrName,record[ATTRCAT_ATTR_NAME_INDEX].sVal);
-	attrCatEntry->attrType=record[ATTRCAT_ATTR_TYPE_INDEX].nVal;
+	attrCatEntry->attrType=record[ATTRCAT_ATTR_TYPE_INDEX].nVal;}
   // copy the rest of the fields in the record to the attrCacheEntry struct
+  //
+
+/* returns the attribute with name `attrName` for the relation corresponding to relId
+NOTE: this function expects the caller to allocate memory for `*attrCatBuf`
+*/
+int AttrCacheTable::getAttrCatEntry(int relId, char attrName[ATTR_SIZE], AttrCatEntry* attrCatBuf) {
+	if(relId<0||relId>=MAX_OPEN) return E_OUTOFBOUND;
+  // check that relId is valid and corresponds to an open relation
+	if(attrCache[relId]==NULL) return E_RELNOTOPEN;
+  // iterate over the entries in the attribute cache and set attrCatBuf to the entry that
+  //    matches attrName
+  struct AttrCacheEntry* attrCacheEntry=attrCache[relId];
+
+  while(attrCacheEntry!=NULL){
+  
+	  if(strcmp(attrCacheEntry->attrCatEntry.attrName,attrName)==0){
+	  	*attrCatBuf=attrCacheEntry->attrCatEntry;
+	  return SUCCESS;
+	  }
+  }
+
+  // no attribute with name attrName for the relation
+  return E_ATTRNOTEXIST;
 }
+
+
+
