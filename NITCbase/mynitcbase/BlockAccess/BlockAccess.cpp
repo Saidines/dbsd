01b5c28498a79248c7e5c55455375322e3a4e292
saidines <saidineshreddykukkala@gmail.com>
2026-01-25 22:50:36 +0530

    temp

--- NITCbase/mynitcbase/BlockAccess/BlockAccess.cpp
+++ NITCbase/mynitcbase/BlockAccess/BlockAccess.cpp
@@ -1,15 +1,18 @@
 #include "BlockAccess.h"
-#include <stdlib.h>
-#include <cstring>
 
-RecId BlockAccess::linearSearch(int relId, char attrName[ATTR_SIZE], union Attribute attrVal, int op) {
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+RecId BlockAccess::linearSearch(int relId, char attrName[ATTR_SIZE], union Attribute attrVal, int op)
+{
+    // TODO: No error handling is done in this function. Should add error handling code.
     // get the previous search index of the relation relId from the relation cache
     // (use RelCacheTable::getSearchIndex() function)
     RecId prevRecId;
     RelCacheTable::getSearchIndex(relId, &prevRecId);
 
-    // let block and slot denote the record id of the record being currently checked
-    int block = -1, slot = -1;
+    int block, slot;
 
     // if the current search index record is invalid(i.e. both block and slot = -1)
     if (prevRecId.block == -1 && prevRecId.slot == -1)
@@ -22,19 +25,22 @@ RecId BlockAccess::linearSearch(int relId, char attrName[ATTR_SIZE], union Attri
         RelCatEntry relCatEntry;
         RelCacheTable::getRelCatEntry(relId, &relCatEntry);
 
-        // block = first record block of the relation
         block = relCatEntry.firstBlk;
-        // slot = 0
         slot = 0;
+
+        // block = first record block of the relation
+        // slot = 0
     }
     else
     {
         // (there is a hit from previous search; search should start from
         // the record next to the search index record)
 
+        // TODO: What if the previous hit is the last record of the relation?
+        // How exactly should I move the block and slot pointers in that case?
         // block = search index's block
-        block = prevRecId.block;
         // slot = search index's slot + 1
+        block = prevRecId.block;
         slot = prevRecId.slot + 1;
     }
 
@@ -49,52 +55,52 @@ RecId BlockAccess::linearSearch(int relId, char attrName[ATTR_SIZE], union Attri
            existing block) */
         RecBuffer recBuffer(block);
 
-        HeadInfo head;
-        Attribute catRecord[RELCAT_NO_ATTRS];
-
         // get the record with id (block, slot) using RecBuffer::getRecord()
-        recBuffer.getRecord(catRecord, slot);
-        // get header of the block using RecBuffer::getHeader() function
+        HeadInfo head;
         recBuffer.getHeader(&head);
+        int numAttrs = head.numAttrs;
+        Attribute record[numAttrs];
+        recBuffer.getRecord(record, slot);
+
+        // get header of the block using RecBuffer::getHeader() function
         // get slot map of the block using RecBuffer::getSlotMap() function
+
         unsigned char slotMap[head.numSlots];
         recBuffer.getSlotMap(slotMap);
 
         // If slot >= the number of slots per block(i.e. no more slots in this block)
-        if (slot >= head.numSlots) {
+        if (slot >= head.numSlots)
+        {
             // update block = right block of block
+            // (use the block header to get the right block of the current block)
             block = head.rblock;
-            // update slot = 0
             slot = 0;
-            continue;  // continue to the beginning of this while loop
+            // update slot = 0
+            continue; // continue to the beginning of this while loop
         }
 
         // if slot is free skip the loop
         // (i.e. check if slot'th entry in slot map of block contains SLOT_UNOCCUPIED)
-        if (slotMap[slot] == SLOT_UNOCCUPIED) {
+        if (slotMap[slot] == SLOT_UNOCCUPIED)
+        {
             // increment slot and continue to the next record slot
             slot++;
             continue;
         }
-
         // compare record's attribute value to the the given attrVal as below:
         /*
-        firstly get the attribute offset for the attrName attribute
-        from the attribute cache entry of the relation using
-        AttrCacheTable::getAttrCatEntry()
+            firstly get the attribute offset for the attrName attribute
+            from the attribute cache entry of the relation using
+            AttrCacheTable::getAttrCatEntry()
         */
         AttrCatEntry attrCatEntry;
         AttrCacheTable::getAttrCatEntry(relId, attrName, &attrCatEntry);
-
-
-        // use the attribute offset to get the value of the attribute from current record
-        Attribute *record =(Attribute *)malloc(sizeof(Attribute) * head.numAttrs);
-        recBuffer.getRecord(record, slot);
-        int attrOffset=attrCatEntry.offset;
-
-        int cmpVal;  // will store the difference between the attributes
+        /* use the attribute offset to get the value of the attribute from
+           current record */
+        Attribute recordAttrVal = record[attrCatEntry.offset];
+        int cmpVal; // will store the difference between the attributes
         // set cmpVal using compareAttrs()
-        cmpVal = compareAttrs(record[attrOffset], attrVal, attrCatEntry.attrType);
+        cmpVal = compareAttrs(attrVal, recordAttrVal, attrCatEntry.attrType);
 
         /* Next task is to check whether this record satisfies the given condition.
            It is determined based on the output of previous comparison and
@@ -102,21 +108,21 @@ RecId BlockAccess::linearSearch(int relId, char attrName[ATTR_SIZE], union Attri
            The following code sets the cond variable if the condition is satisfied.
         */
         if (
-            (op == NE && cmpVal != 0) ||    // if op is "not equal to"
-            (op == LT && cmpVal < 0) ||     // if op is "less than"
-            (op == LE && cmpVal <= 0) ||    // if op is "less than or equal to"
-            (op == EQ && cmpVal == 0) ||    // if op is "equal to"
-            (op == GT && cmpVal > 0) ||     // if op is "greater than"
-            (op == GE && cmpVal >= 0)       // if op is "greater than or equal to"
-        ) {
+            (op == NE && cmpVal != 0) || // if op is "not equal to"
+            (op == LT && cmpVal > 0) ||  // if op is "less than"
+            (op == LE && cmpVal >= 0) || // if op is "less than or equal to"
+            (op == EQ && cmpVal == 0) || // if op is "equal to"
+            (op == GT && cmpVal < 0) ||  // if op is "greater than"
+            (op == GE && cmpVal <= 0)    // if op is "greater than or equal to"
+        )
+        {
             /*
             set the search index in the relation cache as
             the record id of the record that satisfies the given condition
             (use RelCacheTable::setSearchIndex function)
             */
-            RecId newIndex = {block, slot};
-            RelCacheTable::setSearchIndex(relId, &newIndex);
-
+            prevRecId = RecId{block, slot};
+            RelCacheTable::setSearchIndex(relId, &prevRecId);
             return RecId{block, slot};
         }
 
