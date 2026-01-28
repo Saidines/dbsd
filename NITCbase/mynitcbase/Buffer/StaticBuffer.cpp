142050a23ddfa75d3596eb01d131354786370699
saidines <saidineshreddykukkala@gmail.com>
2026-01-28 08:23:59 +0530

    fixed static buffer

--- NITCbase/mynitcbase/Buffer/StaticBuffer.cpp
+++ NITCbase/mynitcbase/Buffer/StaticBuffer.cpp
@@ -6,23 +6,73 @@
 
 
 	
-	int	StaticBuffer::getFreeBuffer(int blockNum){
-		if(blockNum<0||blockNum>DISK_BLOCKS) return E_OUTOFBOUND;
-			int allocatedBuffer=E_OUTOFBOUND;
-		for(int i=0;i<BUFFER_CAPACITY;i++){
-			if(metainfo[i].free==true){
-				allocatedBuffer=i;
-				break;
-		}
-		}
+int StaticBuffer::getFreeBuffer(int blockNum)
+{
+    // Check if blockNum is valid (non zero and less than DISK_BLOCKS)
+    // and return E_OUTOFBOUND if not valid.
+    if (blockNum < 0 || blockNum >= DISK_BLOCKS)
+    {
+        return E_OUTOFBOUND;
+    }
 
-		if(allocatedBuffer!=E_OUTOFBOUND){
-			metainfo[allocatedBuffer].free=false;
-			metainfo[allocatedBuffer].blockNum=blockNum;
-		}
-					
-		return allocatedBuffer;
-	}
+    // increase the timeStamp in metaInfo of all occupied buffers.
+    for (int bufferIndex = 0; bufferIndex < BUFFER_CAPACITY; bufferIndex++)
+    {
+        if (!metainfo[bufferIndex].free)
+        {
+            metainfo[bufferIndex].timeStamp++;
+        }
+    }
+
+    // let bufferNum be used to store the buffer number of the free/freed buffer.
+    int bufferNum;
+    bool bufferFound = false;
+    // iterate through metainfo and check if there is any buffer free
+    for (int bufferIndex = 0; bufferIndex < BUFFER_CAPACITY; bufferIndex++)
+    {
+        if (metainfo[bufferIndex].free)
+        {
+            bufferNum = bufferIndex;
+            bufferFound = true;
+            break;
+        }
+    }
+
+    // if a free buffer is available, set bufferNum = index of that free buffer.
+
+    // if a free buffer is not available,
+    //     find the buffer with the largest timestamp
+    //     IF IT IS DIRTY, write back to the disk using Disk::writeBlock()
+    //     set bufferNum = index of this buffer
+
+    if (!bufferFound)
+    {
+        int maxTimeStamp = -1;
+        for (int bufferIndex = 0; bufferIndex < BUFFER_CAPACITY; bufferIndex++)
+        {
+            if (metainfo[bufferIndex].timeStamp > maxTimeStamp)
+            {
+                maxTimeStamp = metainfo[bufferIndex].timeStamp;
+                bufferNum = bufferIndex;
+            }
+        }
+        if (metainfo[bufferNum].dirty)
+        {
+            Disk::writeBlock(blocks[bufferNum], metainfo[bufferNum].blockNum);
+        }
+    }
+
+    // update the metaInfo entry corresponding to bufferNum with
+    // free:false, dirty:false, blockNum:the input block number, timeStamp:0.
+    metainfo[bufferNum].free = false;
+    metainfo[bufferNum].dirty = false;
+    metainfo[bufferNum].blockNum = blockNum;
+    metainfo[bufferNum].timeStamp = 0; // or -1
+
+    return bufferNum;
+
+    // return the bufferNum.
+}
 
 	int StaticBuffer::getBufferNum(int blockNum){
 		
